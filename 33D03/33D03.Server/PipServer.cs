b060f3f9839fe0f196115d5a2154849d5837d8fd
Parker Jack Kavanagh <120429513+Parkerjackk@users.noreply.github.com>
2024-03-26 17:26:27 +0000

    Update PipServer.cs
    PipServer v1

--- 33D03/33D03.Server/PipServer.cs
+++ 33D03/33D03.Server/PipServer.cs
@@ -1,33 +1,158 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using Nlog;
+using System;
 using System.Text;
-using System.Threading.Tasks;
+using _33D03.Server;
 
-namespace _33D03.Server
+internal class ServerPIP
 {
-    internal class PipServer
+    private TxpServer txpServer;
+
+    private int satResults = 0;
+    private int unsatResults = 0;
+    private int totalClients = 0;
+    private int responses = 0;
+    private double calculation = 0;
+    private string result;
+    private Random random = new Random();
+    private static Logger logger = LogManager.GetCurrentClassLogger();
+
+    public ServerPIP(TxpServer txpServer)
     {
-        private TxpServer txpServer;
+        this.txpServer = txpServer;
+    }
 
-        public PipServer(TxpServer txpServer)
+    public void Start()
+    {
+        txpServer.OnPacketReceived += TxpServer_OnPacketReceived;
+        txpServer.Start();
+        ServerVoteInitHandler();
+    }
+
+    private void TxpServer_OnPacketReceived(TxpClientConversation clientconversation, byte[] data)
+    {
+        string resp = Encoding.UTF8.GetString(data);
+        logger.Info($"Receoved response from client {clientconversation.LasrtEndPoint}: {resp}");
+
+        ProcessResp(resp);
+    }
+
+    private void ServerVoteInitHandler(TxpServer server)
+    {
+        string problem = ProblemGeneration(); //generate a random prob
+        byte[] problemToSend = Encoding.UF8.GetBytes(problem); //convert from string to bits
+        txpServer.send(problemToSend, clientStatus); //send packet in bits
+        logger.Info($"Sent Problem to clients: {problem}");
+    }
+
+    //function to generate problems with random operands and aperators of different lenghts
+    static string ProblemGeneration()
+    {
+
+        int numOperands = random.Next(2, 10); //randomly generate number of operands between 2 and 9
+        List<int> operands = new List<int>(); //list to store operands
+        List<string> operators = new List<string>();//list to store operators
+
+        //Generate and store operands
+        for (int i = 0; i < numOperands; i++)
         {
-            this.txpServer = txpServer;
+            operands.Add(random.Next(-255, 255)); //randomly generate operands
         }
 
-        public void Start()
+        for (int i = 0; i < numOperands - 1; i++)
         {
-            txpServer.OnPacketReceived += TxpServer_OnPacketReceived;
-            txpServer.Start();
+            string[] operators = { "+", "-", "*", "/" };
+            string selectedOp = operators[random.Next(operators.Lenght)];
+            operands.Add(selectedOp); //add selected operators to the list
         }
-
-        private void TxpServer_OnPacketReceived(TxpClientConversation clientconversation, byte[] data)
+        StringBuilder equationBuilder = new StringBuilder();
+        for (int i = 0; i < numOperands - 1; i++)
         {
-            throw new NotImplementedException();
+            equationBuilder.Append(operands[i]);
+            equationBuilder.Append(" ");
+            equationBuilder.Append(operators[i]);
+            equationBuilder.Append(" ");
         }
+        equationBuilder.Append(operands[numOperands - 1]); //append last operand
 
-        private void SendHello()
+        //calculate the total of the equation
+        int total = operands[0];
+        for (int i = 0; i < numOperands - 1; i++)
         {
+            int operand = operands[i + 1];
+            string op = operators[i];
+            switch (op)
+            {
+                case "+":
+                    total += operand;
+                    break;
+                case "-":
+                    total -= operand;
+                    break;
+                case "*":
+                    total *= operand;
+                    break;
+                case "/":
+                    if (operand != 0)
+                    {
+                        total /= operand;
+                    }
+                    else
+                    {
+                        total += random.Next(-10, 10) + 1; //choose a non-zero operand (this will cause the clients to return unsat as they will deal with this issue in dif ways
+                    }
+                    break;
+            }
+        }
+        equationBuilder.Append(" = ");
+        equationBuilder.Append(total);
+
+        return equationBuilder.ToString();
+    }
+
+    private void ProcessResp(string resp)
+    {
+        if (resp.ToLower() == "satisfied")
+        {
+            satResults++;
+        }
+        else if (resp.ToLower() == "unstatisfied")
+        {
+            unsatResults++;
+        }
+        responses++; //increase resp counter
+
+        if (responses == totalClients)
+        {
+            calculateResult();
+        }
+        //This is to reduce the amount of time that the server has to wait if there is already an uncontested winner
+        else if (responses < totalClients)
+        {
+            if (satResults > (totalClients / 2))
+            {
+                result = "Satisfied";
+            }
+            else if (unsatResults > (totalClients / 2))
+            {
+                result = "Unsatisfied";
+            }
+        }
+    }
+
+    private void calculateResult()
+    {
+        int ratio = satResults / totalClients;
+        if (ratio > totalClients / 2)
+        {
+            result = "Satisfied";
+        }
+        else if (ratio < totalClients / 2)
+        {
+            result = "Unstatisfied";
+        }
+        else if (ratio == totalClients / 2)
+        {
+            result = "A TIE??";
         }
     }
 }
