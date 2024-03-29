84758c917af0afa421517273cb34eb46fc7c47f9
Matthew Noble <noblem@tcd.ie>
2024-03-29 09:10:41 +0000

    added more smt generation, should give a mix of both sat and unsat problems

--- 33D03/33D03.Client/PipClient.cs
+++ 33D03/33D03.Client/PipClient.cs
@@ -21,23 +21,70 @@ namespace _33D03.Client
         static string GenerateSMTLIBString()
         {
             StringBuilder smtBuilder = new StringBuilder();
-            // Start with setting the logic
-            smtBuilder.Append("(set-logic QF_LIA) ");
-            // Declare two integer constants
-            smtBuilder.Append("(declare-const x Int) (declare-const y Int) ");
-            // Decide randomly to generate a SAT or UNSAT expression
-            bool generateSAT = random.Next(0, 2) == 0;
-            if (generateSAT)
+            string[] diffLogics = new string[] { "QF_LIA", "QF_BV" };           //randomises the logic used between linear integer arithmetic or bit vectors
+            string selectedLogic = diffLogics[random.Next(diffLogics.Length)];
+
+            smtBuilder.Append($"(set-logic {selectedLogic})\n");
+
+            //declares constants based off selected logic
+            if (selectedLogic == "QF_LIA")
             {
-                // Generate a likely SAT example
-                int a = random.Next(1, 10);
-                smtBuilder.AppendFormat("(assert (= x (+ y {0}))) ", a);
+                smtBuilder.Append("(declare-const x Int) (declare-const y Int)\n");
             }
-            else
+            else if (selectedLogic == "QF_BV")
             {
-                // Generate a likely UNSAT example
-                smtBuilder.Append("(assert (= (- x y) (+ x (- y) 1))) ");
+                smtBuilder.Append("(declare-const a (_ BitVec 32)) (declare-const b (_ BitVec 32))\n");
             }
+
+            //randomises type of SMT problem created by the generator
+            //should produce mix of SAT and UNSAT
+            int diffProblems = random.Next(4);
+            if (diffProblems == 0)
+            {
+                int randVal = random.Next(1, 1000);
+                if (selectedLogic == "QF_LIA")  //these comparisons will likely be SAT
+                {
+                    smtBuilder.AppendFormat("(assert (> (+ x y) {0}))\n", randVal);
+                }
+                else if (selectedLogic == "QF_BV")
+                {
+                    smtBuilder.AppendFormat("(assert (bvslt (bvadd a b) #x{0:X8}))\n", randVal);
+                }
+            }
+            else if (diffProblems == 1)         //these logical expressions should also likely be SAT
+            {
+                if (selectedLogic == "QF_LIA")
+                {
+                    smtBuilder.AppendLine("(assert (or (> x 1) (< y 15)))");
+                }
+                else
+                {
+                    smtBuilder.AppendLine("(assert (bvor (bvslt a #x0000000A) (bvsgt b #x000000AA)))");
+                }
+            }
+            else if (diffProblems == 2)         //these conditions should always be UNSAT as there is no constant values that should satisfy
+            {
+                if (selectedLogic == "QF_LIA")
+                {
+                    smtBuilder.AppendLine("(assert (and (> x 70) (< x 40)))");
+                }
+                else
+                {
+                    smtBuilder.AppendLine("(assert (and (bvslt a #x000000044) (bvsgt a #x000000067)))");
+                }
+            }
+            else if (diffProblems == 3)         //more conditions that will likely be SAT that include some multiplication and addition
+            {
+                if (selectedLogic == "QF_LIA")
+                {
+                    smtBuilder.AppendLine("(assert (= (+ x (* 2 y)) 50))");
+                }
+                else
+                {
+                    smtBuilder.AppendLine("(assert (bvsle (bvadd a b) #xF000000F))");
+                }
+            }
+
             // Finish with checking satisfiability
             smtBuilder.Append("(check-sat)");
             return smtBuilder.ToString();
